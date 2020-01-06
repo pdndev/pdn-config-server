5c6a3049320ea290a0c0c01d6748f6bb17688939
Arunkumar Arjunan <arunkumar.a@prodapt.com>
2020-01-06 08:03:33 +0530

    added

---
+++ src/main/java/com/pro/app/PdnConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.pro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdnConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdnConfigServerApplication.class, args);
+	}
+
+}
